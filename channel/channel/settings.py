17469436dcb5e5b3b62e3f6887eb319e2c374882
Leo Agea <leoagea@gmail.com>
2025-01-28 16:05:49 +0100

    Update: basic hello world page

--- channel/channel/settings.py
+++ channel/channel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l60sdlw!ngfq*9ix0a%*3$!1loyc9e*s!m&^#p(ii%k3u(e=f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	
+    'chat',
 ]
 
 MIDDLEWARE = [
--- channel/channel/urls.py
+++ channel/channel/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from chat.views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('', hello_world, name="hello_world"),
 ]
