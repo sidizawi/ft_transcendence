afe67a7c0fac282073483bdc955958ca6d61da38
Leo Agea <leoagea@gmail.com>
2025-02-24 19:14:43 +0100

    DB not working yet

---
+++ services/auth-service/db.js
@@ -0,0 +1,17 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const Database = require('better-sqlite3');
+
+const db = new Database('data/test.sqlite', { verbose: console.log });
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    game_data TEXT DEFAULT '{}'
+    );
+`);
+
+export default db;
