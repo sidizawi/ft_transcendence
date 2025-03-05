59ba84f2a35fe6dd6316193fe0149860eb21f46c
Leo Agea <leoagea@gmail.com>
2025-03-05 00:26:30 +0000

    add friend fonction ok

--- services/chat-service/index.js
+++ services/chat-service/index.js
@@ -3,20 +3,78 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import Fastify    from 'fastify';
+import fastifyJwt from '@fastify/jwt';
 import db         from './db.js';
 
 const fastify = Fastify({ logger: false });
 fastify.addHook('onResponse', (request, reply, done) => {
-    console.log(`${request.method} ${request.url} ${reply.statusCode}${reply.message}`);
+    console.log(`${request.method} ${request.url} ${reply.statusCode}`);
     done();
 });
 
+await fastify.register(fastifyJwt, { secret: process.env.JWT_SECRET })
+
 fastify.decorate('db', db);
 
-fastify.listen({ port: 3001, host: '0.0.0.0' }, (err, address) => {
+// todo: update friend list
+fastify.post('/add', async (request, reply) =>{
+	const { username } = request.body;
+	if (!username){
+		reply.code(400);
+		return { error: 'Username needed'};
+	}
+
+	await request.jwtVerify();
+	const actualuser = request.user.username;
+	const actualid = request.user.id;
+
+	if (actualuser === username){
+		reply.code(400)
+		return { error: 'Cannot add yourself'}
+	}
+
+	const userExists = fastify.db.prepare("SELECT * FROM users WHERE username = ?").get(username);
+	if (!userExists) {
+		reply.code(400);
+		return { error: 'Username doesnt exist'};
+	}
+
+	const friendid = userExists.id;
+
+	const friend = fastify.db.prepare("SELECT * FROM friend where id = ?").get(actualid);
+	
+	if (!friend) {
+		const newObj = { [friendid]: 'sending' };
+		db.prepare('INSERT INTO friend (id, list) VALUES (?, ?)').run(
+		  actualid,
+		  JSON.stringify(newObj)
+		);
+		reply.code(201);
+		return { message: 'User successfully added'};
+	}
+
+	const friendlist = JSON.parse(friend.list);
+	console.log(friendlist)
+	if (friendid in friendlist){
+		reply.code(400);
+		return { error: 'Username already in friendlist'};
+	}
+
+	friendlist[friendid] = 'sending';
+  
+  	const update = db.prepare("UPDATE friend SET list = ? WHERE id = ?");
+	update.run(JSON.stringify(friendlist), actualid);
+  
+	reply.code(201);
+	return { message: 'User successfully added'};
+});
+
+//todo: accept friend request, remove friend, block friend
+
+fastify.listen({ port: 3003, host: '0.0.0.0' }, (err, address) => {
 	if (err) {
 	  fastify.log.error(err);
 	  process.exit(1);
 	}
-	console.log(`🔑 Auth Service running at ${address}`);
+	console.log(`🔑 Chat Service running at ${address}`);
 });
--- services/db/entrypoint.sh
+++ services/db/entrypoint.sh
@@ -2,26 +2,30 @@
 
 DB_PATH="/app/data/management.db"
 
-#echo "Vérification de la présence de $DB_PATH ..."
-
-#if [ ! -f "$DB_PATH" ]; then
-#    echo "La base n'existe pas encore, création..."
-    
-    # On crée la base et la table
-    sqlite3 "$DB_PATH" <<EOF
+sqlite3 "$DB_PATH" <<EOF
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    game_data TEXT DEFAULT '{}'
+    status BOOLEAN NOT NULL CHECK (status IN (0, 1)) DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS friend (
+    id INTEGER,
+    list TEXT DEFAULT '{}',
+    FOREIGN KEY (id) REFERENCES users(id)
 );
 EOF
 
-#    echo "Base et table 'users' créées."
-#else
-#    echo "La base existe déjà, rien à faire."
-#fi
+#JSON friends in friendlist
+    # id of your friend
+    # status : sending, receiving, friend
+#Exemple: if we are id = 1
+    #{2:'sending', 4:'friend', 8:receiving}
+    #we need id 2 to accept/decline our request
+    #we are friend with id 4
+    #we need to accept/decline from id 8
+
 
 # Pour garder le conteneur en vie:
 exec tail -f /dev/null
