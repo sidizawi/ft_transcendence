55370121a02140633a85c4100ea936caa6253726
Clara Aymard <caymard@shi-r9-p3.s19.be>
2025-03-27 14:56:48 +0100

    resolve merge conflcts

--- services/db/entrypoint.sh
+++ services/db/entrypoint.sh
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     game_data TEXT DEFAULT '{}',
-    is_two_factor_enabled INTEGER DEFAULT 0
+    is_two_factor_enabled INTEGER DEFAULT 0,
     status BOOLEAN NOT NULL CHECK (status IN (0, 1)) DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS friend (
