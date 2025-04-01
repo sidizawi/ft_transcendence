7f1795f267be919cbbf5dfc6e53a70e633e97529
Leo AGEA <leoagea@gmail.com>
2025-04-01 17:06:12 +0200

    refactor: gamehistory and gamestats made gamehistory and gamehistory/game into 1 methode same for gamestats

--- services/user-service/routes/profile.js
+++ services/user-service/routes/profile.js
@@ -4,13 +4,17 @@ dotenv.config();
 
 async function profileRoutes(fastify, options) {
 
+	/*
+	 * Oppement: The opponent's username
+	 * score: The score of the game in the format "userScore-opponentScore"
+	 * game: The type of game played, either pong or p4
+	*/
 	fastify.post('/savegamestat', async (request, reply) => {
 		const { opponent, score, game } = request.body;
 		await request.jwtVerify();
 
 		const userId = request.user.id;
 		const userName = request.user.username;
-		//   const user = await db.query('SELECT * FROM users WHERE id = $1', [userId]); TODO refactor
 
 		const opponentExist = fastify.db.prepare('SELECT * FROM users WHERE username = ?').get(opponent);
 		if (!opponentExist) {
@@ -39,40 +43,24 @@ async function profileRoutes(fastify, options) {
 		reply.code(201).send({ message: 'Game saved' });
   	});
 
-	fastify.get('/gameshistory', async (request, reply) => {
-		await request.jwtVerify();
-		const userId = request.user.id;
-
-		const games = fastify.db.prepare('SELECT * FROM game WHERE playerid_1 = ? OR playerid_2 = ? ORDER BY date DESC').all(userId, userId);
-
-		if (games.length === 0 || !games) {
-			return reply.code(404).send({ error: 'No games found' });
-		}
-		
-		if (games.length > 10){
-			games.splice(10);
-		}
-		
-		const formattedGames = games.map(game => {
-			return {
-				id: game.id,
-				opponent: game.playerid_1 === userId ? game.username_2 : game.username_1,
-				score: game.playerid_1 === userId ? `${game.score_1}-${game.score_2}` : `${game.score_2}-${game.score_1}`,
-				playerWin: game.playerid_1 === userId ? game.player_win : game.player_lost,
-				game: game.game_type,
-				date: game.date
-			};
-		});
-
-		reply.code(200).send(formattedGames);
-  	});
-
-	fastify.get('/gameshistory/:game', async (request, reply) => {
+	fastify.get('/gameshistory/:game?', async (request, reply) => {
 		await request.jwtVerify();
 		const userId = request.user.id;
 		const gameType = request.params.game;
 
-		const games = fastify.db.prepare('SELECT * FROM game WHERE (playerid_1 = ? OR playerid_2 = ?) AND game_type = ? ORDER BY date DESC').all(userId, userId, gameType);
+		let query = 'SELECT * FROM game WHERE (playerid_1 = ? OR playerid_2 = ?)';
+		let params = [userId, userId];
+
+		if (gameType !== 'pong' && gameType !== 'p4') {
+			return reply.code(400).send({ error: 'Invalid game type' });
+		}
+
+		if (gameType) {
+			query += ' AND game_type = ?';
+			params.push(gameType);
+		}
+		query += ' ORDER BY date DESC';
+		const games = fastify.db.prepare(query).all(...params);
 		
 		if (games.length === 0 || !games) {
 			return reply.code(404).send({ error: 'No games found' });
@@ -96,32 +84,24 @@ async function profileRoutes(fastify, options) {
 		reply.code(200).send(formattedGames);
   	});
 
-	fastify.get('/gamestats', async (request, reply) => {
-		await request.jwtVerify();
-		const userId = request.user.id;
-
-		const games = fastify.db.prepare('SELECT * FROM game WHERE playerid_1 = ? OR playerid_2 = ?').all(userId, userId);
-
-		if (games.length === 0 || !games) {
-			return reply.code(404).send({ error: 'No games found' });
-		}
-
-		const stats = {
-			totalGames: games.length,
-			wins: games.filter(game => game.player_win === (game.playerid_1 === userId ? game.username_1 : game.username_2)).length,
-			losses: games.filter(game => game.player_lost === (game.playerid_1 === userId ? game.username_1 : game.username_2)).length,
-			winrate: 0
-		};
-		stats.winrate = stats.totalGames > 0 ? Math.round((stats.wins / stats.totalGames) * 100) : 0;
-		
-		reply.code(200).send(stats);
-	});
-
-	fastify.get('/gamestats/:game', async (request, reply) => {
+	fastify.get('/gamestats/:game?', async (request, reply) => {
 		await request.jwtVerify();
 		const userId = request.user.id;
 		const gameType = request.params.game;
-		const games = fastify.db.prepare('SELECT * FROM game WHERE (playerid_1 = ? OR playerid_2 = ?) AND game_type = ?').all(userId, userId, gameType);
+
+		let query = 'SELECT * FROM game WHERE playerid_1 = ? OR playerid_2 = ?';
+		let params = [userId, userId];
+
+		if (gameType !== 'pong' && gameType !== 'p4') {
+			return reply.code(400).send({ error: 'Invalid game type' });
+		}
+
+		if (gameType) {
+			query += ' AND game_type = ?';
+			params.push(gameType);
+		}
+
+		const games = fastify.db.prepare(query).all(...params);
 
 		if (games.length === 0 || !games) {
 			return reply.code(404).send({ error: 'No games found' });
