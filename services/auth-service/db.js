65041c03fc4bea8ba4c118b0ef5642612a188838
Leo AGEA <leoagea@gmail.com>
2025-04-25 19:41:17 +0200

    refactor: remove database connection files from auth and chat services

--- services/auth-service/db.js
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const dbPath = process.env.DB_PATH || './management';
-const db = new Database(dbPath);
-
-export default db;
